639515caaf0a99af208618c5325237923984e317
Mina.Mousa <Mina.Mousa@MINA-MOUSA>
2025-02-04 13:41:48 +0200

    Initial commit

--- JavaScript/config.js
+++ JavaScript/config.js
@@ -1,1 +1,5 @@
- 
+const config = {
+    apiUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+        ? 'http://localhost:3000'
+        : 'https://your-backend-url.com'
+}; 
